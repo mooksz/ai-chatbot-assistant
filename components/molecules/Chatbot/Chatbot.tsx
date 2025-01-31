7bac5e5cd5e76dc9759037c1347ff1023e04b8d7
Mikey Haklander <mikey.haklander@capgemini.com>
2025-01-31 14:08:28 +0100

    Change layout

--- components/molecules/Chatbot/Chatbot.tsx
+++ components/molecules/Chatbot/Chatbot.tsx
@@ -1,4 +1,4 @@
-import { useMemo, type FC } from "react";
+import { type FC } from "react";
 import { Chatbot as ChatbotType } from "@/types/chatbot";
 import { Avatar } from "@/components/atoms/Avatar/Avatar";
 import { Button } from "@/components/ui/button";
@@ -10,25 +10,11 @@ export const Chatbot: FC<Readonly<ChatbotProps>> = (props) => {
   const { name, id, created_at, chatbot_characteristics, chat_sessions } =
     props;
 
-  const date = useMemo(() => {
-    return (
-      new Date(created_at).toLocaleDateString("nl-NL") +
-      " - " +
-      new Date(created_at).toLocaleTimeString("nl-NL")
-    );
-  }, [created_at]);
-
   return (
     <div className="rounded-lg bg-white border p-5">
-      <div className="flex items-start gap-3">
+      <div className="flex items-center gap-3">
         <Avatar className="w-12 h-12 relative" seed={`chatbot-${id}`} />
-        <div>
-          <h2 className="font-bold text-xl">{name}</h2>
-          <p className="text-sm text-gray-500">
-            Created: {date} <br />
-            Sessions: {chat_sessions?.length || 0}
-          </p>
-        </div>
+        <h2 className="font-bold text-xl">{name}</h2>
 
         <Button asChild className="ml-auto">
           <Link href={`/edit-chatbot/${id}`}>Edit chatbot</Link>
@@ -56,6 +42,17 @@ export const Chatbot: FC<Readonly<ChatbotProps>> = (props) => {
         <Button asChild className="ml-auto mt-3" variant={"secondary"}>
           <Link href={`/edit-chatbot/${id}`}>Edit chatbot characteristics</Link>
         </Button>
+      </div>
+
+      <hr className="mt-5 mb-3" />
+
+      <div className="flex justify-between items-center">
+        <p className="text-sm text-gray-500">
+          Sessions: {chat_sessions?.length || 0}
+        </p>
+        <p className="text-sm text-gray-500">
+          Created: {new Date(created_at).toLocaleString("nl-NL")}
+        </p>
       </div>
     </div>
   );
